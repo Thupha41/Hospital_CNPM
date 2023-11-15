cf362821cd23e87cdf9f50d404d920d2b6016ebf
yv.fooh <hntvy23@gmail.com>
2023-11-15 23:09:39 +0700

    Cashier View Incompleted + Assets

--- HospitalManagement/Program.cs
+++ HospitalManagement/Program.cs
@@ -11,7 +11,12 @@ namespace HospitalManagement
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
+
+            //Test cashier view
+            //Application.Run(new CashierView.Cashier_Dashboard());
+
+            Application.Run(new CashierView.Cashier());
         }
     }
 }
